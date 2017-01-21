173d938933aef67b6bdde6f5d9b19bbd3b2a74c2
Gerrix <jahja.trifunovic@gmail.com>
2017-01-21 20:14:57 +0100

    TextView is reqistered.

--- app/src/main/java/com/example/gerrix/versioncontrol/MainActivity.java
+++ app/src/main/java/com/example/gerrix/versioncontrol/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.gerrix.versioncontrol;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView probaTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        probaTv = (TextView) findViewById(R.id.probaTv);
     }
 }
